6b5c8bad1bece598162c3c5e32fbd443fec97859
Koushik <paponkoushik@gmail.com>
2023-12-20 23:58:23 +0600

    artical migration and model added with uuid and relation established with categories

---
+++ app/Models/Article.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Support\Str;
+
+class Article extends Model
+{
+    protected $fillable = ['slug', 'title', 'content', 'author', 'published_at'];
+
+    protected $keyType = 'string';
+    public $incrementing = false;
+    protected $primaryKey = 'id';
+
+    public static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            $model->id = (string) Str::uuid();
+        });
+    }
+
+    public function author(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'author');
+    }
+
+    public function categories(): BelongsToMany
+    {
+        return $this->belongsToMany(Category::class);
+    }
+}
---
+++ database/migrations/2023_12_20_165906_create_articles_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('slug')->unique();
+            $table->string('title')->index();
+            $table->string('content');
+            $table->unsignedBigInteger('author');
+            $table->timestamp('published_at')->nullable();
+            $table->timestamps();
+
+            $table->foreign('author')
+                ->references('id')
+                ->on('users')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('articles');
+    }
+};
